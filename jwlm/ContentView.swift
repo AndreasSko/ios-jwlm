7b3557780b599b4f6f8fb506503db24e16bcb640
Andreas Skorczyk <me@andreas-sk.de>
2020-11-12 16:41:28 +0100

    :lipstick: Shadow for cards
    Makes it easier to visually distinguish the cards
    from the rest of the app. Also just looks nice :)

--- jwlm/ContentView.swift
+++ jwlm/ContentView.swift
@@ -30,11 +30,13 @@ struct ContentView: View {
             }
 
             MergeSettingsView(jwlmController: jwlmController)
+                .background(Color.white)
                 .cornerRadius(10)
                 .overlay(
                     RoundedRectangle(cornerRadius: 10)
                         .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
                 )
+                .shadow(color: Color.gray.opacity(0.2), radius: 20)
                 .padding([.top, .horizontal])
 
             MergeView(jwlmController: jwlmController)
