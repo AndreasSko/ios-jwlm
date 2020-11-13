2e258a6dff6cc983e65f6d11ada1a99f8f8db8c9
Andreas Skorczyk <me@andreas-sk.de>
2020-11-13 16:17:07 +0100

    More info in BookmarkOverview
    BookNumber, ChapterNumber, DocumentID, and
    Track might be helpful when solving a Bookmark
    conflict.

--- jwlm/MergeConflictOverview.swift
+++ jwlm/MergeConflictOverview.swift
@@ -42,6 +42,42 @@ struct BookmarkOverview: View {
                 .padding(.bottom, 0.5)
             }
 
+            if related?.location?.bookNumber.valid ?? false {
+                HStack {
+                    Text("Book number:").bold()
+                    Text("\(related?.location?.bookNumber.int32 ?? -1)")
+                        .alignmentGuide(.custom) { $0[.leading] }
+                }
+                .padding(.bottom, 0.5)
+            }
+
+            if related?.location?.chapterNumber.valid ?? false {
+                HStack {
+                    Text("Chapter number:").bold()
+                    Text("\(related?.location?.chapterNumber.int32 ?? -1)")
+                        .alignmentGuide(.custom) { $0[.leading] }
+                }
+                .padding(.bottom, 0.5)
+            }
+
+            if related?.location?.documentId.valid ?? false {
+                HStack {
+                    Text("Document ID:").bold()
+                    Text(String(related?.location?.documentId.int32 ?? -1))
+                        .alignmentGuide(.custom) { $0[.leading] }
+                }
+                .padding(.bottom, 0.5)
+            }
+
+            if related?.location?.track.valid ?? false {
+                HStack {
+                    Text("Track:").bold()
+                    Text("\(related?.location?.track.int32 ?? -1)")
+                        .alignmentGuide(.custom) { $0[.leading] }
+                }
+                .padding(.bottom, 0.5)
+            }
+
             HStack(alignment: .top) {
                 Text("Slot:").bold()
                 Text("\(bookmark.slot)").alignmentGuide(.custom) { $0[.leading] }
